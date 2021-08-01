2db106aa1d384409baf412239ad0ddd094813365
Mike Sowers <msowers72@yahoo.com>
2021-08-01 14:12:54 -0400

    added more code to index.js

--- index.js
+++ index.js
@@ -3,22 +3,31 @@ const fs = require('fs');
 const util = require('util');
 const axios = require('axios');
 const jest = require('jest');
+const Manager = require("./lib/manager")
+const Engineer = require("./lib/engineer")
+const Intern = require("./lib/intern")
+const Employee = require("./lib/employee")
+
 
 const writeFileAsync = util.promisify(fs.writeFile);
+let teamArray =[]
+let managerArray =[]
+let engineerArray =[]
+let internArray = []
+let employeeArray = [] 
 
-
-const promptUser = () => {
+const createManager = () => {
     return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'What is your name?',
-        },
+        },     
         {
             type: 'input',
-            name: 'role',
-            message: 'What is your role?',
-        },
+            name: 'employeeid',
+            message: 'What is your employee ID?',
+        },   
         {
             type: 'input',
             name: 'email',
@@ -26,18 +35,82 @@ const promptUser = () => {
         },
         {
             type: 'input',
-            name: 'officeid',
-            message: 'What is your office Id?',
+            name: 'number',
+            message: 'What is your office number?',
         },
-        {
-            type: 'input',
-            name: 'employeeid',
-            message: 'What is your employee ID?',
-        },
+        
 
     ]);
 
 };
+
+const addTeamMembers = function() {
+    inquirer
+    .prompt([
+    {
+        type: "list",
+        name: "choice",
+        message: "What type of employee would you like to create?",
+        choices: ["Manager", "Engineer", "Intern"]
+    }])
+}
+
+const createEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },     
+        {
+            type: 'input',
+            name: 'employeeid',
+            message: 'What is your employee ID?',
+        },   
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'number',
+            message: 'What is your office number?',
+        },
+        
+
+    ]);
+
+};
+
+const createIntern = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },     
+        {
+            type: 'input',
+            name: 'employeeid',
+            message: 'What is your employee ID?',
+        },   
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'number',
+            message: 'What is your office number?',
+        },
+        
+
+    ]);
+
+};
+
 const generateHTML = (answers) =>
     `<!DOCTYPE html>
 <html lang="en">
@@ -154,10 +227,95 @@ const init = () => {
       // - engineer - getEngineerInfo()
       // - intern  - getInternInfo()
       // -all done - writeFile(manager, engineers, interns)
-    promptUser()
-        .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
-        .then(() => console.log('Successfully wrote to index.html'))
-        .catch((err) => console.error(err));
+    createManager()
+        .then((answers) => {
+            console.log(answers)
+            const manager = new Manager(answers.name, answers.employeeid, answers.email, answers.number)
+            console.log(manager);
+            const engineer = new Engineer(answers.name, answers.employeeid, answers.email, answers.number)
+            console.log(engineer)
+            //push manager to array
+            teamArray.push(manager)
+            console.log(teamArray)
+            teamArray.push(engineer)
+            console.log(teamArray)
+            addTeamMembers();
+            
+
+          // writeFileAsync('index.html', generateHTML(answers))
+        })
+         //.then(() => console.log('Successfully wrote to index.html'))
+         //.catch((err) => console.error(err));
 };
 
 init();
+
+
+
+/*
+- collect manager info
+- prompt for more users
+  - engineer
+  - intern
+
+all done?
+  - write file to html
+  writeFileAsync(manager, engineers, interns) {
+      const managerHTML = (manager) => {
+         return `
+                <div class="card employee-card">
+                    <div class="card-header">
+                        <h2 class="card-title">${manager.name}</h2>
+                        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.role}</h3>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">email: ${manager.email}</li>
+                            <li class="list-group-item">Office ID: ${manager.officeid}</li>
+                            <li class="list-group-item">Employee ID: ${manager.employeeid}</li>
+                        </ul>
+                    </div>            
+                </div>
+      `
+      };
+
+      const engineersHTML = (engineers) => {
+          let returnString = "";
+
+          if (engineers && engineers.length > 0) {
+              engineers.forEach(engineer => {
+                  returnString += `
+                <div class="card employee-card">
+                    <div class="card-header">
+                        <h2 class="card-title">${engineer.name}</h2>
+                        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.role}</h3>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">email: ${engineer.email}</li>
+                            <li class="list-group-item">Office ID: ${engineer.officeid}</li>
+                            <li class="list-group-item">Employee ID: ${engineer.employeeid}</li>
+                        </ul>
+                    </div>            
+                </div>
+                  `
+              })
+          }
+
+          return returnString;
+      }
+
+      const finalOut = `
+      <html>
+        <body>
+            <div class="cards">
+            ${managerHTML(manager)}
+            ${engineersHTML(engineers)}
+            ${interns}
+            </div>
+        </body>
+      </html>
+      `
+  }
+
+*/
